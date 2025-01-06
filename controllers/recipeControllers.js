9b92460cd89266bc143522c57eb6ab6dd453d6c9
Njahi98 <njahioussama75@gmail.com>
2025-01-06 22:45:10 +0100

    added remaining controllers and details page

--- controllers/recipeControllers.js
+++ controllers/recipeControllers.js
@@ -1,46 +1,57 @@
-const Recipe = require('../models/recipe')
+const Recipe = require("../models/recipe");
 
-
-const recipe_index = (req,res)=>{
-    Recipe.find()
-    .then((result)=>{
-        res.render('recipes/index',{title:'recipes',recipes:result})
-    }).catch((error)=>{
-        console.log(error);
+const recipe_index = (req, res) => {
+  Recipe.find()
+    .then((result) => {
+      res.render("recipes/index", { title: "recipes", recipes: result });
     })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-}
+const recipe_create_get = (req, res) => {
+  res.render("recipes/create", { title: "create a new recipe" });
+};
 
-const recipe_create_get = (req,res)=>{
-    res.render("recipes/create", { title: "create a new recipe" });
-}
-
-const recipe_create_post = (req,res) =>{
-    const recipe = new Recipe();
-    recipe.save()
-    .then((result)=>{
-        res.redirect("/recipes");
-    }).catch((error)=>{
-        console.log(error);
+const recipe_create_post = (req, res) => {
+  const recipe = new Recipe();
+  recipe
+    .save()
+    .then((result) => {
+      res.redirect("/recipes");
     })
-}
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-const recipe_details = (req,res)=>{
+const recipe_details = async (req, res) => {
+  const id = req.params.id;
+  Recipe.findById(id)
+    .then((result) => {
+      res.render("recipes/details", {
+        recipe: result,
+        title: "recipe details",
+      });
+    })
+    .catch((err) => {
+      res.status(404).render("404", { title: "recipe not found" });
+    });
+};
 
-}
+const recipe_delete = (req, res) => {
+    const id=req.params.id;
+    Recipe.findByIdAndDelete(id)
+    .then(result=>{
+        res.json({redirect:'/recipes'})
+    }).catch(err=>console.log(err))
+};
 
-const recipe_delete = (req,res) =>{
-
-}
-
-
-
-
-
-module.exports={
-    recipe_index,
-    recipe_create_get,
-    recipe_create_post,
-    recipe_details,
-    recipe_delete
-}
+module.exports = {
+  recipe_index,
+  recipe_create_get,
+  recipe_create_post,
+  recipe_details,
+  recipe_delete,
+};

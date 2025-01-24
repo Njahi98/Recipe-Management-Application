e4fc7daa80166972d1297b93240574747083dfa3
Njahi98 <njahioussama75@gmail.com>
2025-01-24 23:37:05 +0100

    added modal for recipe deletion

--- public/scripts/recipes-details.js
+++ public/scripts/recipes-details.js
@@ -1,12 +1,40 @@
+
 const trashcan = document.querySelector(".delete-btn");
 
+const modal = document.createElement('div');
+modal.classList.add('modal-backdrop')
 trashcan.addEventListener("click", (e) => {
-  const endpoint = `/recipes/${trashcan.dataset.doc}`;
-
-  fetch(endpoint, {
-    method: "DELETE",
+  modal.innerHTML=
+  `
+  <div class="modal-container">
+  <h3>Are you sure?</h3>
+  <p>Do you really want to delete this recipe? This action cannot be undone.</p>
+<div class="modal-actions">
+  <button class="modal-confirm-action">Confirm</button>
+  <button class="modal-cancel-action">Cancel</button>
+</div>
+</div>
+  `
+  document.body.appendChild(modal);
+  const confirmBtn=document.querySelector('.modal-confirm-action');
+  const cancelBtn=document.querySelector('.modal-cancel-action');
+  confirmBtn.addEventListener('click',()=>{
+    const endpoint = `/recipes/${trashcan.dataset.doc}`;
+  
+    fetch(endpoint, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => (window.location.href = data.redirect))
+      .catch((err) => console.log(err));
+  
   })
-    .then((response) => response.json())
-    .then((data) => (window.location.href = data.redirect))
-    .catch((err) => console.log(err));
+  cancelBtn.addEventListener('click',()=>{
+    modal.remove();
+  })
+  
 });
+
+
+
+

e6d1689e1b3732e6afbcd731bf4e5c3670f1fa88
Njahi98 <njahioussama75@gmail.com>
2025-02-28 20:30:24 +0100

    moved profile edit script to public folder

---
+++ public/scripts/profile-edit.js
@@ -0,0 +1,52 @@
+const form = document.querySelector(".edit-profile-container form");
+      const inputs = form.querySelectorAll("textarea, input");
+
+      form.addEventListener("submit", async function (e) {
+        e.preventDefault();
+        try {
+          // if there is an image, use formData
+          const formData = new FormData(form);
+
+          if (formData.get('image').size > 0) {
+
+          const response = await fetch(`/profile/${profileId}`, {
+          method:"PUT",
+          body:formData,
+        });
+        if(response.ok){
+         window.location.href=`/profile/${profileId}`
+        }else {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+          }else{
+            //there is no new image, use this formData
+            const jsonData = {
+            bio: form.bio.value,
+            location: form.location.value,
+            website: form.website.value,
+            socialMedia: {
+            twitter: form.twitter.value,
+            instagram: form.instagram.value,
+            facebook: form.facebook.value,
+          },
+        };
+        const response = await fetch(`/profile/${profileId}`, {
+          method:"PUT",
+          headers: {'content-type':'application/json',            
+          },
+          body:JSON.stringify(jsonData),
+        });
+        if(response.ok){
+         window.location.href=`/profile/${profileId}`
+        }else {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+          }
+        } catch (error) {
+          showNotification(error,'var(--error-color)');
+        }
+
+        
+      });

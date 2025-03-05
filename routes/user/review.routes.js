f2d33112391b5e0dbcbca7f09db43c23905e5889
Njahi98 <njahioussama75@gmail.com>
2025-03-05 22:10:38 +0100

    allow guests to review recipes

--- routes/user/review.routes.js
+++ routes/user/review.routes.js
@@ -6,7 +6,7 @@ const reviewControllers = require('../../controllers/user/review.controller')
 const router = express.Router();
 
 
-router.post('/:id/reviews',isAuthenticated,reviewControllers.recipe_add_review)
+router.post('/:id/reviews',reviewControllers.recipe_add_review)
 
 router.put('/:recipeId/reviews/:reviewId',isAuthenticated,isReviewOwner,reviewControllers.recipe_update_review)
 
